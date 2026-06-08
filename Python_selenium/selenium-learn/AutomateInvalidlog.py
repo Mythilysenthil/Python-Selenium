cd8540ea9474a41594123e4dcadc9edf07762542
Mythilysenthil <mythilsenthilp@gamil.com>
2026-06-08 19:29:47 +0530

    Modify the AutomateValidLog

--- Python_selenium/selenium-learn/AutomateInvalidlog.py
+++ Python_selenium/selenium-learn/AutomateInvalidlog.py
@@ -3,12 +3,20 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.options import Options
 
-driver = webdriver.Chrome()
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-gpu")
+options.add_argument("--window-size=1920,1080")
+
+driver = webdriver.Chrome(options=options)
 wait = WebDriverWait(driver,timeout=20,poll_frequency=2,ignored_exceptions=[NoSuchElementException])
 
 driver.maximize_window()
-driver.get("http://automationexercise.com")
+driver.get("https://automationexercise.com")
 
 home = wait.until(EC.visibility_of_element_located((By.XPATH,"//a[normalize-space()='Home']")))
 home.is_displayed()
